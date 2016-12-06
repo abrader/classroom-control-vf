c2fa822ffe8d2db24c57890f26361683da23302c
Ilya Shaisultanov <ilya.shaisultanov@gmail.com>
2016-12-06 12:44:45 -0800

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -43,4 +43,10 @@ node default {
   # Example:
   #   class { 'my_class': }
   include role::classroom
+  file { 'etc/motd': 
+    ensure => file,
+    group => '0',
+    mode => '0644',
+    owner => 'root'
+  }
 }
