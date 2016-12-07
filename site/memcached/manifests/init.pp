24bca42614b60b0bb9ed449fd511dccf11d5df2c
Zack Penka & Daniel Berry <zpenka@shutterstock.com>
2016-12-07 14:15:31 -0700

    Wrong directory :/

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -1,5 +1,21 @@
-PORT="11211"
-USER="memcached"
-MAXCONN="96"
-CACHESIZE="32"
-OPTIONS=""
+class memcached {
+  package { 'memcached':
+    ensure => present,
+  }
+
+  file { '/etc/sysconfig/memcached' :
+    ensure  => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    path    => 'etc/sysconfig/memcached',
+    source  => 'puppet:///modules/memcached/config',
+    require => Package['memcached'],
+  }
+
+  service { 'memcached.service':
+    ensure    => running,
+    enable    => true,
+    subscribe => File['/etc/sysconfig/memcached'],
+  }
+}
