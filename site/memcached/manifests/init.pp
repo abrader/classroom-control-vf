38734076307349899a1fcd961d23de67cbd2cb82
lxming <xiaoming.liu@gmail.com>
2016-12-07 16:13:25 -0500

    add memcached

---
+++ site/memcached/manifests/init.pp
@@ -0,0 +1,16 @@
+ class { 'memcached':
+     package { 'memcached':
+      ensure => present,
+     }
+     file { '/etc/sysconfig/memcached':
+     path =>'sysconfig/memcached',
+     content =>'PORT="11211"\nUSER="memcached"\nMAXCONN="96"\nCACHESIZE="32"\nOPTIONS="\n'
+     require => Package['memcached'],
+     }
+     service {'memcached':
+      ensure =>running,
+      enable =>true,
+      subscribe => File['/etc/sysconfig/memcached']
+     }
+   }
+      
