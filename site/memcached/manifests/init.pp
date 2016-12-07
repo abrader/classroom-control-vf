58e20d92a8c61387e4ef9deaf9e1b270be50562a
nmartynov1 <nmartynov@shutterstock.com>
2016-12-07 15:58:00 -0500

    add memcached manifest

---
+++ site/memcached/manifests/init.pp
@@ -0,0 +1,21 @@
+class memcached {
+  package { 'memcached':
+    ensure => present,
+    before => File['/etc/sysconfig/memcached'],
+  }
+  
+  file { '/etc/sysconfig/memcached':
+    ensure  => 'file',
+    source  => 'puppet:///modules/memcached/memcached',
+    group   => 'root',
+    mode    => '0644',
+    owner   => 'root',
+    notify  => Service['memcached'],
+  }
+ 
+  service { 'memcached.service':
+    ensure => 'running',
+    enable => 'true',
+    require => [Package['memcached'], File['/etc/sysconfig/memcached']],
+  }
+}
