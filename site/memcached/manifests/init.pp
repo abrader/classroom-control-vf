93accdbfaafd0c1ffee718f7728c03751a05082a
Geoff Galitz <geoff@galitz.org>
2016-12-07 22:20:15 +0100

    Update init.pp
    added class declaration for memcached

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -1,19 +1,20 @@
-package { 'memcached':
-  ensure => present,
-}
+class memcached {
+  package { 'memcached':
+    ensure => present,
+  }
 
-file { 'memcached_conf':
-  path    => '/etc/sysconfig/memcached'
-  ensure  => 'file',
-  content => '{md5}a79f5d50f8632f06682af7269bdd0505',
-  group   => '0',
-  mode    => '0644',
-  owner   => '0',
-}
+  file { 'memcached_conf':
+    path    => '/etc/sysconfig/memcached'
+    ensure  => 'file',
+    content => '{md5}a79f5d50f8632f06682af7269bdd0505',
+    group   => '0',
+    mode    => '0644',
+    owner   => '0',
+  }
 
-service { 'memcached':
-  enable => true,
-  ensure => running,
-  subscribe => File['/etc/sysconfig/memcached'],
+  service { 'memcached':
+    enable => true,
+    ensure => running,
+    subscribe => File['/etc/sysconfig/memcached'],
+  }
 }
-
