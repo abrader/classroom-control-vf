63e80d7a7592ade98db8e7b4cc8e7842b75f24f9
Andrew Brader <abrader@gmail.com>
2016-12-07 14:10:48 -0500

    Create init.pp
    Managing the /etc/skel directory and /etc/skel/.bashrc file respectively.

---
+++ site/skeleton/manifests/init.pp
@@ -0,0 +1,18 @@
+class skeleton {
+  file { 'skel_dir' :
+    ensure => directory,
+    path   => '/etc/skel',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0755',
+  }
+  
+  file { 'skel_bashrc':
+    ensure  => 'file',
+    path    => '/etc/skel/.bashrc',
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    source  => 'puppet:///modules/skeleton/bashrc',
+  }
+}
