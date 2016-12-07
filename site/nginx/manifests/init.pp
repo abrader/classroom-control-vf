0a4483496b70402acb8c4c24b1b904fa38a8368f
Ilya Shaisultanov <ishaisultanov@shutterstock.com>
2016-12-07 13:21:43 -0800

    Hardcode docroot

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -11,7 +11,7 @@ class nginx {
   file {'index.html':
     ensure   => file,
     source   => 'puppet:///modules/nginx/index.html',
-    path     => File['docroot']['path'],
+    path     => '/var/www',
     require  => File['docroot'],
   }
 
