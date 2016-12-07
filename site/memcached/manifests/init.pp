56f219a47a941802b93a25a92bdb164d41c27b08
Geoff Galitz <geoff@galitz.org>
2016-12-07 22:38:48 +0100

    Update init.pp
    Removed content parameter from memcached module

--- site/memcached/manifests/init.pp
+++ site/memcached/manifests/init.pp
@@ -6,7 +6,6 @@ class memcached {
   file { 'memcached_conf':
     path    => '/etc/sysconfig/memcached'
     ensure  => 'file',
-    content => '{md5}a79f5d50f8632f06682af7269bdd0505',
     group   => '0',
     mode    => '0644',
     owner   => '0',
