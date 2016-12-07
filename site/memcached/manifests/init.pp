4c73b5b09dc440bf63df6c3071459e3153c82c9c
rkrzyminskishutterstock <rkrzyminski@shutterstock.com>
2016-12-07 16:05:04 -0500

    11.1: Package | File | Service
    added memcache package file and service

---
+++ site/memcached/manifests/init.pp
@@ -0,0 +1,26 @@
+class memcache { 
+
+  package { 'memcached' :
+  ensure => present,
+  }
+  
+  file { 'file-memcached' :
+    ensure => file,
+    owner   => 'root',
+    group   => 'root',
+    mode    => '0644',
+    content => '#managed by Puppet\
+    PORT="11211"\
+    USER="memcached"\
+    MAXCONN="96"\
+    CACHESIZE="32"\
+    OPTIONS=""',
+    require => Package['memcached'],
+  }
+  
+  service { 'memcached' :
+    ensure => running,
+    enable => true,
+    subscribe => File['file-memcached'],  
+  }
+}
