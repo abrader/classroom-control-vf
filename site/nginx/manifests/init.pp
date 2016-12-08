d91fcc02f094dbf4ba551ed374ff425bb8e63fb8
Andrew Brader <abrader@puppet.com>
2016-12-08 14:54:03 +0000

    Added index.html
    In this commit I added a file resource to manage index.html

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -2,7 +2,7 @@ class nginx {
   package { 'nginx' :
     ensure => present,
   }
-  
+
   file { 'nginx_conf' :
     ensure => file,
     path   => '/etc/nginx/nginx.conf',
@@ -12,7 +12,7 @@ class nginx {
     source => 'puppet:///modules/nginx/nginx.conf',
     notify => Service['nginx'],
   }
-  
+
   file { 'default_conf' :
     ensure => file,
     path   => '/etc/nginx/conf.d/default.conf',
@@ -22,7 +22,14 @@ class nginx {
     source => 'puppet:///modules/nginx/default.conf',
     notify => Service['nginx'],
   }
-  
+
+  file { 'index_page' :
+    ensure => file,
+    path   => '/var/www/index.html',
+    mode   => '0644',
+    source => 'puppet:///modules/nginx/index.html',
+  }
+
   service { 'nginx' :
     ensure => running,
     enable => true,
