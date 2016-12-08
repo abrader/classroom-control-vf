4599e608e8ee0a364352fba246ed7eb056912116
Andrew Brader <abrader@gmail.com>
2016-12-08 13:55:05 -0500

    Create managed_user.pp
    Created managed_users defined resource type

---
+++ site/users/manifests/managed_user.pp
@@ -0,0 +1,13 @@
+define users::managed_users (
+  $group = $title,
+) {
+  user { $title :
+    ensure => present,
+  }
+  
+  file { "/home/${title}" :
+    ensure => directory,
+    owner  => $title,
+    group  => $group,
+  }
+}
