f7a2741fa84d8c36d9132c1430d64e1a92a3bbd6
Andrew Brader <abrader@puppet.com>
2016-12-08 20:08:24 +0000

    Fixed a bunch of typos

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,30 +1,30 @@
 class nginx {
   case $::osfamily {
-    'redhat','debian : {
-      $package = 'nginx',
-      $owner   = 'root',
-      $group   = 'root',
-      $docroot = '/var/www',
-      $confdir = '/etc/nginx',
-      $logdir  = '/var/log/nginx',
+    'redhat','debiani' : {
+      $package = 'nginx'
+      $owner   = 'root'
+      $group   = 'root'
+      $docroot = '/var/www'
+      $confdir = '/etc/nginx'
+      $logdir  = '/var/log/nginx'
     }
     'windows' : {
-      $package = 'nginx-service',
-      $owner   = 'Administrator',
-      $group   = 'Administrators',
-      $docroot = 'C:/ProgramData/nginx/html',
-      $confdir = 'C:/ProgramData/nginx/',
-      $logdir  = 'C:/ProgramData/nginx/logs',
+      $package = 'nginx-service'
+      $owner   = 'Administrator'
+      $group   = 'Administrators'
+      $docroot = 'C:/ProgramData/nginx/html'
+      $confdir = 'C:/ProgramData/nginx/'
+      $logdir  = 'C:/ProgramData/nginx/logs'
     }
     default : {
       fail("Module ${module_name} is not supported for use on ${::osfamily}")
     }
   }
   
-  $user = $::osfamily {
-    'redhat'  = 'nginx',
-    'debian'  = 'www-data',
-    'windows' = 'nobody',
+  $user = $::osfamily ? {
+    'redhat'  => 'nginx',
+    'debian'  => 'www-data',
+    'windows' => 'nobody',
   }
 
   File {
