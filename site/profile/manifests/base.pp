39ee6c29060ac3bcde3d22b96e40d42ec07d30d5
Geoff Galitz <geoff@galitz.org>
2016-12-08 21:14:09 +0100

    Update base.pp
    boom

--- site/profile/manifests/base.pp
+++ site/profile/manifests/base.pp
@@ -1,6 +1,10 @@
 class profile::base {
   notify { "Hello, my name is ${::hostname}": }
   #$msg => hiera('info','I am base.pp!')
-  notify { hiera('message', 'I am base.pp!'): }
+  
+  #notify { hiera('message', 'I am base.pp!'): }
   #notify { "Hello again.. ${msg}": }
+  
+  $message = hiera('message')
+  notify { "Here is my message: ${message}" : }
 }
