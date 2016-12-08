09a658add78538d5d72c64155b0fdaa0196f6257
nmartynov1 <nmartynov@shutterstock.com>
2016-12-08 09:42:15 -0500

    add index.html

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -2,4 +2,28 @@ class nginx {
   package { 'nginx':
     ensure => present
   }
+ 
+  file { '/var/www/':
+    ensure => 'directory',
+    group  => 'root',
+    mode   => '0755',
+    owner  => 'root',
+    require => Package['nginx'],
+  }
+
+ 
+  file { '/var/www/index.html':
+    ensure  => 'file',
+    source  => 'puppet:///modules/nginx/index.html',
+    group   => 'root',
+    mode    => '0644',
+    owner   => 'root',
+    #notify  => Service['memcached.service'],
+  }
+
+  service { 'nginx.service':
+    ensure => 'running',
+    enable => 'true',
+    require => [Package['nginx'], File['/var/www/index.html']],
+  }
 }
