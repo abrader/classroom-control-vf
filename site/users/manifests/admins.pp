4b0da9c857da96d2a605f83e57eefad0d1b7f4df
nmartynov1 <nmartynov@shutterstock.com>
2016-12-08 14:00:49 -0500

    fix

--- site/users/manifests/admins.pp
+++ site/users/manifests/admins.pp
@@ -1,5 +1,5 @@
 class users::admins {
-  managed_user { 'jose' : }
-  managed_user { 'alice' : }
-  managed_user { 'chen' : }
+  users::managed_user { 'jose': }
+  users::managed_user { 'alice': }
+  users::managed_user { 'chen': }
 }
