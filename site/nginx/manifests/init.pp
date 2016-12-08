71cb61b96ef16fefc5eed3eee0ab086922ea371c
Zack Penka & Daniel Berry <zpenka@shutterstock.com>
2016-12-08 07:41:53 -0700

    Base class for nginx

---
+++ site/nginx/manifests/init.pp
@@ -0,0 +1,30 @@
+class nginx {
+  package { 'nginx' :
+    ensure => present,
+  }
+
+  file { 'nginx_conf' :
+    ensure => file,
+    path   => '/etc/nginx/nginx.conf',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+    source => 'puppet:///modules/nginx/nginx.conf',
+    notify => Service['nginx'],
+  }
+
+  file { 'default_conf' :
+    ensure => file,
+    path   => '/etc/nginx/conf.d/default.conf',
+    owner  => 'root',
+    group  => 'root',
+    mode   => '0644',
+    source => 'puppet:///modules/nginx/default.conf',
+    notify => Service['nginx'],
+  }
+
+  service { 'nginx' :
+    ensure => running,
+    enable => true,
+  }
+}
