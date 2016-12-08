8ff8412becfa9c188367cab7079b7c56b23c5bfd
rkrzyminskishutterstock <rkrzyminski@shutterstock.com>
2016-12-08 13:54:58 -0500

    added .ssh dir

--- site/users/manifests/managed_user.pp
+++ site/users/manifests/managed_user.pp
@@ -5,7 +5,14 @@ define users::managed_user (
   user { $title:
     ensure => present, 
   }
+  
   file { "/home/${title}":
+    ensure => directory,
+    owner => $title,
+    group => $group,
+  }
+  
+  file { "/home/.ssh":
     ensure => directory,
     owner => $title,
     group => $group,
